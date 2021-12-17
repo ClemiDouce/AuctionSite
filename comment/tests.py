d3cca0e20cec09f3efa5651a70c24db089bb6fab
Quentin Laparra <quentin.laparra@gmail.com>
2021-12-17 11:37:46 +0100

    add model comment content test

--- comment/tests.py
+++ comment/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from account.models import CustomUser
+from auction.models import Auction
+from comment.models import Comment
 
 # Create your tests here.
+
+
+class TestComment(TestCase):
+    """"""
+    def setUp(self):
+        user = CustomUser.object.create(username='Usertest', 
+                                        email='test@test.fr',
+                                        password='motdepasse445')
+        auction = Auction.objects.create(title='AuctionTest', base_price=12, author=user)
+        comment = Comment.objects.create(content="je fais un test", author=user, auction=auction)
+        comment.save()
+        
+    def test_comment_model(self):
+        """test the model content text"""
+        comment_test = Comment.objects.get(content="je fais un test")
+        self.assertEqual(comment_test.content, "je fais un test")
+       
