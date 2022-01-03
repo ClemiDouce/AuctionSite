756954fe5b4a7cb7eae097787e6311867a8d6e18
Quentin Laparra <quentin.laparra@gmail.com>
2022-01-03 15:57:49 +0100

    modif path for check comment

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('auction.urls'))
+    path('', include('auction.urls')),
+    path('comment/', include('comment.urls'))
 ]
