7c4a91854e7916e788731904a4bff83f8d66b749
Mathieu <dupycournau@gmail.com>
2022-01-03 14:42:29 +0100

    add detail tempalte

--- auction/views.py
+++ auction/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "auction/basetemplate.html")
+    return render(request, "auction/auction_detail.html")
