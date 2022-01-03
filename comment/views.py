6d3d830bdf7bcc26e4f24a35fb6968c1537d1e7c
Quentin Laparra <quentin.laparra@gmail.com>
2022-01-04 00:00:04 +0100

    add function create comment

--- comment/views.py
+++ comment/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from comment.models import Comment
+
+from .forms import NewCommentForm
 
 # Create your views here.
 def comment_view(request):
-    return render(request, "comment/comment_details.html")
+    comments = Comment.objects.all()
+    return render(request, "comment/comment_details.html", {'comments': comments})
+
+
+def create_comment(request):
+    """ Function to create a comment for an auction"""
+    
+    if request.method == "POST":
+        form = NewCommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.save()
+            return redirect('comment')
+    else:
+        form = NewCommentForm()        
+
+    #form = NewCommentForm()
+    return render(request, 'comment/create_comment.html', {'form': form})
