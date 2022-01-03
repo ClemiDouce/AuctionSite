2503d89d3cd101dd9581f67f761b67447ef4fc81
Quentin Laparra <quentin.laparra@gmail.com>
2022-01-04 00:00:35 +0100

    create path for new comment

--- comment/urls.py
+++ comment/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import comment_view
+from .views import comment_view, create_comment
 
 urlpatterns = [
-    path('', comment_view)
+    path('', comment_view, name='comment' ),
+    path('new-comment/', create_comment, name='new-comment')
 ]
