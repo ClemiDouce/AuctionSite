412e7be3ef5c7b8c7f5c53a35ec82079db3160fa
Quentin Laparra <quentin.laparra@gmail.com>
2022-01-04 15:11:03 +0100

    PEP8

--- comment/urls.py
+++ comment/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import CommentAuctionView, CommentAuctionCreate
 
 app_name = 'comment'
@@ -8,4 +10,4 @@ urlpatterns = [
     path('', CommentAuctionView.as_view(), name='comment' ),
     path('new-comment/', CommentAuctionCreate.as_view(), name='new-comment'),
     #path('new-comment/', create_comment, name='new-comment')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- comment/views.py
+++ comment/views.py
@@ -4,28 +4,6 @@ from comment.models import Comment
 
 from .forms import NewCommentForm
 
-# Create your views here.
-# def comment_view(request):
-#     comments = Comment.objects.all()
-#     return render(request, "comment/comment_details.html", {'comments': comments})
-    
-
-# def create_comment(request):
-#     """ Function to create a comment for an auction"""
-    
-#     if request.method == "POST":
-#         form = NewCommentForm(request.POST)
-#         if form.is_valid():
-#             new_comment = form.save(commit=False)
-#             new_comment.author = request.user
-#             new_comment.save()
-#             return redirect('comment')
-#     else:
-#         form = NewCommentForm()        
-
-#     #form = NewCommentForm()
-#     return render(request, 'comment/create_comment.html', {'form': form})
-
 
 class CommentAuctionView(ListView):
     model = Comment
@@ -37,7 +15,7 @@ class CommentAuctionCreate(CreateView):
     model = Comment
     form_class = NewCommentForm
     template_name = "comment/create_comment.html"
-    #fields = ['auction', 'content']
+    
     
     def form_valid(self, form):
         form.instance.author = self.request.user
