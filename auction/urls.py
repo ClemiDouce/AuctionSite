840b71a457560838ca2dc06af5a7d0d6d8c988fe
clem <dupycournau@gmail.com>
2022-01-06 15:57:37 +0100

    add comment space to auction detail

--- auction/urls.py
+++ auction/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import index
+from .views import index, auction_detail
 
 urlpatterns = [
-    path('', index)
+    path("", index),
+    path("<int:auction_id>", auction_detail)
 ]
--- auction/views.py
+++ auction/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 from .models import Auction
+from comment.models import Comment
 
 # Create your views here.
 def index(request):
     auction_list = Auction.objects.all()
     return render(request, "auction/auction_list.html", {"auction_list": auction_list})
+
+def auction_detail(request, auction_id):
+    auction = Auction.objects.get(id=auction_id)
+    comments = Comment.objects.filter(auction=auction)
+    return render(request, "auction/auction_detail.html", {"auction": auction, "comments": comments})
