3c11beb9c0ca70c6916769074ee780854cff6608
clem <dupycournau@gmail.com>
2022-01-11 15:48:39 +0100

    add bulma style in navbar

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
+
+def test(request):
+    return render(request, "auction/basetemplate.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("test", test),
     path('', include('auction.urls')),
     path('comment/', include('comment.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
